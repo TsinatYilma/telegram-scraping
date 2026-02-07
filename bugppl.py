0f10767ffbf4c706825549a1228f8f9de6e6532c
Tsinat <tsinatyilma16@gmail.com>
2026-02-07 19:13:24 +0300

    bug ppl

---
+++ bugppl.py
@@ -0,0 +1,43 @@
+
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
+from dotenv import load_dotenv
+load_dotenv() 
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+client = TelegramClient("anon", api_id, api_hash)
+
+async def main():
+    await client.start()
+    await client.send_message('me', "Man this is amazing. i will not judge hackers from now on,/n cuz this feels amazing")
+    print("✅ Message sent to Saved Messages")
+    chat = '@kinfe123'
+    messages = ["MoltBot, Testing, \nSurprise! Automation loves you 😎 \nEven if you get sick hearing the word AI \n and sponsor me am going to die!😶"]
+    repeat_count = 100
+    delay = 0.2  
+    delete_after = 10000
+    # collect all deletion tasks
+    deletion_tasks = []
+    for i in range(repeat_count):
+        for msg in messages:
+            sent_msg = await client.send_message(chat, msg)
+            print(f"Message {i+1} sent ✌️")
+            # schedule deletion+++++++++
+            task = asyncio.create_task(delete_later(chat, sent_msg, delete_after))
+            deletion_tasks.append(task)
+            await asyncio.sleep(delay) 
+    # wait for all deletions to complete
+    await asyncio.gather(*deletion_tasks)
+    print("All messages sent and deleted!")
+async def delete_later(chat, message, delay):
+    await asyncio.sleep(delay)
+    await client.delete_messages(chat, message, revoke=True)
+    print(f"😒 Message deleted")
+# run the main function
+asyncio.run(main())
+
+with client:
+    client.loop.run_until_complete(main())
